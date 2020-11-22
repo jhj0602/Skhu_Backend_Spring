940d7d218497b780cf2a056d140285efa2524e49
jhj960918 <65895403+jhj960918@users.noreply.github.com>
2020-11-23 00:58:21 +0900

    resultmap2 구현 Lombok활용

---
+++ mybatis3b/src/main/java/net/skhu/dto/Register.java
@@ -0,0 +1,15 @@
+package net.skhu.dto;
+
+import java.util.Date;
+import lombok.Data;
+
+@Data
+public class Register {
+	int id;
+	int studentId;
+	int courseId;
+	int grade;
+	Date createDate;
+	Student student;
+	Course course;
+}
