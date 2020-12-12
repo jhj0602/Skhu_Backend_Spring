4cf203d73192e5979a72e10792e3b4ae49e3f702
jhj960918 <65895403+jhj960918@users.noreply.github.com>
2020-12-12 19:48:09 +0900

    11/30 구현시험 jpa활용

---
+++ 구현시험/E201534027/src/main/java/net/skhu/E201534027Application.java
@@ -0,0 +1,13 @@
+package net.skhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E201534027Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E201534027Application.class, args);
+	}
+
+}
---
+++ 구현시험/E201534027/src/test/java/net/skhu/E201534027ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.skhu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E201534027ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
