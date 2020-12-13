4987c991d7e22d2bcec5266a0554dea09972ed40
jhj960918 <65895403+jhj960918@users.noreply.github.com>
2020-12-13 20:01:13 +0900

    JPA 상품 카테고리별 출력product3.sql 응용

--- JPA_응용 구현/jpa3_Product/src/main/java/net/skhu/repository/CityRepository.java
+++
@@ -1,7 +0,0 @@
-package net.skhu.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import net.skhu.entity.City;
-
-public interface CityRepository extends JpaRepository<City, Integer> {
-}
